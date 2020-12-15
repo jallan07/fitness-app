b2c587e3c1a2a45c0a088385643d3403414819e2
Josh Allan <allan.josh07@gmail.com>
2020-12-15 16:52:43 -0500

    Working to get the add/update api path to work.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,17 @@
-let db = require('../models');
+const db = require('../models');
+const mongojs = require('mongojs');
 
 module.exports = (app) => {
   //TODO create the api routes
   //get all workouts for home page
   app.get('/api/workouts', (req, res) => {
-    db.Workouts.find({}).then(function (workouts) {
+    db.Workouts.find({}).then((workouts) => {
       res.json(workouts);
     });
   });
   // get all workouts for workout dashboard
   app.get('/api/workouts/range', (req, res) => {
-    db.Workouts.find({}).then(function (workouts) {
+    db.Workouts.find({}).then((workouts) => {
       res.json(workouts);
     });
   });
@@ -25,5 +26,13 @@ module.exports = (app) => {
         if (err) console.log(err);
       });
   });
-  //
+
+  // get and then update a workout
+  app.get('/exercise:?id', (req, res) => {
+    let workoutID = mongojs.ObjectId(req.query.id);
+    console.log(workout._id);
+    app.put('/api/workouts', (req, res) => {
+      db.Workouts.update({ _id: workoutID });
+    });
+  });
 };
